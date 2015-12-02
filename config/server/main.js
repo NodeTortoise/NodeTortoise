046364399269553a3d5aa5852074aea6f2a75f7f
omartinez-namu <oscarmartinez@goduesouth.com>
2015-12-02 12:09:09 -0600

    Added index info. Changed Galapagos address

--- config/server/main.js
+++ config/server/main.js
@@ -3,5 +3,5 @@ GLOBAL.CONFIG = {
     DEBUG_MODE: 1, // 1, 2, 3, 4
     SESSION_CHECK_INACTIVITY_MINUTES: 5,
     SESSION_MAX_INACTIVITY_MINUTES: 15,
-    GALAPAGOS_URL: 'http://mygalapagos.com/create-standalone'
+    GALAPAGOS_URL: 'http://52.70.122.198/create-standalone'
 };
